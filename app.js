ab71f23ac7b7ca2de53f8d723b93b06fab3dc827
Skyz03 <aakibdiscoo@gmail.com>
2022-02-21 10:37:06 +0545

    Updates in the challange

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-const timeNodes = Array.from(document.querySelectorAll('[date-time]'));
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+const seconds = timeNodes
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(':').map(parseFloat);
+    return (mins * 60) + secs;
+  })
+  .reduce((total, vidSeconds) => total + vidSeconds);
+
+let secondsLeft = seconds;
+
+const hours = Math.floor(secondsLeft / (60 * 60)); //seconds divided by 60 into mins then divided by 60 takes it to hours.
+
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(seconds);
+console.log(hours, mins, secondsLeft);
+
+
 
